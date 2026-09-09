bab9f8922ff03bbc2785a90a30dfe16b1be65c86
cuuw619-maker <cuuw619@gmail.com>
2026-09-09 23:24:22 +0300

    Fix OpenGL constants for real Liquid Glass renderer

--- src/main/java/restudio/reglass/client/render/LiquidGlassRenderer.java
+++ src/main/java/restudio/reglass/client/render/LiquidGlassRenderer.java
@@ -5,6 +5,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
@@ -67,7 +68,7 @@ public final class LiquidGlassRenderer {
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
 
-        GL11.glActiveTexture(GL11.GL_TEXTURE0);
+        GL13.glActiveTexture(GL13.GL_TEXTURE0);
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, snapshotTexture);
         GL20.glUseProgram(program);
         GL30.glBindVertexArray(vao);
@@ -163,8 +164,8 @@ public final class LiquidGlassRenderer {
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, snapshotTexture);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_CLAMP_TO_EDGE);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_CLAMP_TO_EDGE);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL13.GL_CLAMP_TO_EDGE);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL13.GL_CLAMP_TO_EDGE);
         GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, (ByteBuffer) null);
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 
