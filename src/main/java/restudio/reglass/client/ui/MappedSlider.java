cdd27d18fc3a034447ace3889ed4eae67fb365a9
cuuw619-maker <cuuw619@gmail.com>
2026-09-09 22:16:07 +0300

    Migrate ReGlass slider widget to NeoForge 1.21.1 GUI mappings

--- src/main/java/restudio/reglass/client/ui/MappedSlider.java
+++ src/main/java/restudio/reglass/client/ui/MappedSlider.java
@@ -1,61 +1,63 @@
 package restudio.reglass.client.ui;
 
 import java.util.function.Consumer;
-import net.minecraft.client.gui.widget.SliderWidget;
-import net.minecraft.text.Text;
-import restudio.reglass.mixin.accessor.SliderWidgetAccessor;
+import net.minecraft.client.gui.components.AbstractSliderButton;
+import net.minecraft.network.chat.Component;
 
-public class MappedSlider extends SliderWidget {
-
+public class MappedSlider extends AbstractSliderButton {
     private final double min;
     private final double max;
     private final Consumer<Double> onChange;
     private final boolean integer;
-    private final Text originalMessage = this.getMessage();
+    private final Component label;
 
-    public static MappedSlider floatSlider(int x, int y, int width, int height, Text msg, double min, double max, double init, Consumer<Double> onChange) {
+    public static MappedSlider floatSlider(int x, int y, int width, int height, Component msg,
+                                           double min, double max, double init, Consumer<Double> onChange) {
         return new MappedSlider(x, y, width, height, msg, min, max, init, onChange, false);
     }
 
-    public static MappedSlider intSlider(int x, int y, int width, int height, Text msg, int min, int max, int init, Consumer<Integer> onChange) {
-        return new MappedSlider(x, y, width, height, msg, min, max, init, d -> onChange.accept(d.intValue()), true);
+    public static MappedSlider intSlider(int x, int y, int width, int height, Component msg,
+                                         int min, int max, int init, Consumer<Integer> onChange) {
+        return new MappedSlider(x, y, width, height, msg, min, max, init,
+                d -> onChange.accept(d.intValue()), true);
     }
 
-    private MappedSlider(int x, int y, int width, int height, Text message, double min, double max, double init, Consumer<Double> onChange, boolean integer) {
-        super(x, y, width, height, message, 0);
+    private MappedSlider(int x, int y, int width, int height, Component message,
+                         double min, double max, double init, Consumer<Double> onChange, boolean integer) {
+        super(x, y, width, height, message, inverse(init, min, max));
         this.min = min;
         this.max = max;
         this.onChange = onChange;
         this.integer = integer;
-        ((SliderWidgetAccessor) this).setValuePublic(inverseMap(init));
+        this.label = message;
         updateMessage();
     }
 
-    private double map(double v) {
-        return min + v * (max - min);
+    private static double inverse(double real, double min, double max) {
+        if (max == min) return 0;
+        return Math.max(0, Math.min(1, (real - min) / (max - min)));
     }
 
-    private double inverseMap(double real) {
-        if (max == min) return 0;
-        return (real - min) / (max - min);
+    private double map(double value) {
+        return min + value * (max - min);
     }
 
     @Override
     protected void updateMessage() {
-        double v = map(this.value);
-        if (integer) v = Math.round(v);
-        this.setMessage(Text.literal(originalMessage.getString() + ": " + format(v)));
-    }
-
-    private String format(double v) {
-        if (integer) return Integer.toString((int) Math.round(v));
-        return String.format("%.3f", v);
+        double value = map(this.value);
+        if (integer) value = Math.round(value);
+        this.setMessage(Component.literal(label.getString() + ": " + format(value)));
     }
 
     @Override
     protected void applyValue() {
-        double v = map(this.value);
-        if (integer) v = Math.round(v);
-        onChange.accept(v);
+        double value = map(this.value);
+        if (integer) value = Math.round(value);
+        onChange.accept(value);
+    }
+
+    private String format(double value) {
+        if (integer) return Integer.toString((int) Math.round(value));
+        return String.format(java.util.Locale.ROOT, "%.3f", value);
     }
 }
